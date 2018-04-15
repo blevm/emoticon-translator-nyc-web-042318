4b2bec7ef4bae69a02f36628a8694d99f71a0d60
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 21:02:41 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,15 +6,12 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   emoticons.each do |meaning, emoticon_list|
     emoticons_hash = {}
-    binding.pry
     emoticons_hash[:get_meaning] = {}
     emoticons_hash[:get_emoticon] = {}
-    binding.pry
     emoticons_hash[:get_meaning][emoticon_list[1]] = meaning
     emoticons_hash[:get_emoticon][emoticon_list[0]] = emoticon_list[1]
-    binding.pry
   end
-  emoticons
+  emoticons_hash
 end
 
 def get_japanese_emoticon
