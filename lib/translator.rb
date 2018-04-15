72165cfee4afbb57f7c356d29a9ff6d3f3dd34fa
Michelle Blevins <mjblevins7@gmail.com>
2018-04-15 21:01:04 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,8 +10,8 @@ def load_library(file_path)
     emoticons_hash[:get_meaning] = {}
     emoticons_hash[:get_emoticon] = {}
     binding.pry
-    get_meaning[emoticon_list[1]] = meaning
-    get_emoticon[emoticon_list[0]] = emoticon_list[1]
+    emoticons_hash[:get_meaning][emoticon_list[1]] = meaning
+    emoticons_hash[:get_meaning][emoticon_list[0]] = emoticon_list[1]
     binding.pry
   end
   emoticons
